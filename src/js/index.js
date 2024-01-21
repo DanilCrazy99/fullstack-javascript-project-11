7002103b2e9777bdd5ae8d22c2e37ada0a2aedd7
Danil Volkov <volchara287@gmail.com>
2024-01-21 23:22:38 +0400

    feat: add validation url in input

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,41 @@
 // Import our custom CSS
 import '../scss/styles.scss';
+import onChange from 'on-change';
+import validation from './validation.js';
+
+const state = {
+  items: [],
+  input: {
+    state: 'valid',
+  },
+};
+
+const inputEl = document.querySelector('input#urlInput');
+const btnEl = document.querySelector('button');
+
+const proxyState = onChange(state, (path, value) => {
+  if (path === 'input.isValid') {
+    const map = {
+      valid: () => {
+        inputEl.classList.remove('is-invalid');
+      },
+      invalid: () => {
+        inputEl.classList.add('is-invalid');
+      },
+    };
+    map[value]();
+  }
+});
+
+btnEl.addEventListener('click', (e) => {
+  e.preventDefault();
+  try {
+    const { value } = inputEl;
+    validation(value).then((result) => {
+      proxyState.input.isValid = result ? 'valid' : 'invalid';
+    });
+    console.log(state);
+  } catch (err) {
+    console.error(err);
+  }
+});
---
+++ src/js/validation.js
@@ -0,0 +1,10 @@
+import * as yup from 'yup';
+
+let schema = yup.object().shape({
+  url: yup.string().url().required(),
+});
+
+export default (value) => schema
+  .validate({ url: value }, { strict: true })
+  .then(({ url }) => url)
+  .catch(console.error);
