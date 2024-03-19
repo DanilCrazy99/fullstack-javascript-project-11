fc05bcd987642375af1c865a81e488e7fe3264ae
Danil Volkov <volchara287@gmail.com>
2024-03-20 00:33:08 +0400

    add: добавляет локализацию сообщений об ошибках при помощи i18n

--- src/js/view/watcher.js
+++ src/js/view/watcher.js
@@ -1,30 +1,37 @@
 import onChange from 'on-change';
+import i18next from 'i18next';
 import userScheme from '../validation.js';
+import ru from '../lang/ru.js';
+
+i18next.init({
+  lng: 'ru',
+  debug: true,
+  resources: {
+    ru,
+  },
+});
 
 const infoBlock = document.getElementById('info-block');
 export const inputEl = document.getElementById('url-input');
 
 export const appState = onChange(
   {
-    value: '',
+    value: null,
     urls: [],
     error: '',
     info: [],
   },
   (path, value) => {
-    // console.log(path);
-    // console.log(value);
+    // console.log('path:', path);
+    // console.log('value:', value);
     // console.log(prevValue);
     // console.log(applyData);
     const feedBackEl = document.createElement('p');
     switch (path) {
       case 'error': {
-        const dict = {
-          doubleUrl: 'В списке уже есть такой URL',
-          invalidUrl: 'Некорректный URL',
-        };
+        if (value === null) return;
         feedBackEl.classList.add('text-danger', 'm-0');
-        feedBackEl.textContent = dict[value];
+        feedBackEl.textContent = i18next.t(`feedback.errors.${value}`);
         infoBlock.replaceChildren(feedBackEl);
         inputEl.classList.add('is-invalid');
         break;
@@ -35,23 +42,22 @@ export const appState = onChange(
           appState.error = 'doubleUrl';
         } else {
           userScheme
-            .isValid({ value })
+            .validate({ value })
             .then((result) => {
-              if (result) {
-                appState.error = '';
-                inputEl.classList.remove('is-invalid');
-                appState.urls.push(value);
-                feedBackEl.classList.add('text-success', 'm-0');
-                feedBackEl.textContent = 'URL успешно добавлен';
-                infoBlock.replaceChildren(feedBackEl);
-                inputEl.value = '';
-                inputEl.focus();
-                appState.value = null;
-              } else {
-                appState.error = 'invalidUrl';
-              }
+              appState.error = null;
+              inputEl.classList.remove('is-invalid');
+              appState.urls.push(value);
+              feedBackEl.classList.add('text-success', 'm-0');
+              feedBackEl.textContent = i18next.t('feedback.info.urlAdded');
+              infoBlock.replaceChildren(feedBackEl);
+              inputEl.value = '';
+              inputEl.focus();
+              appState.value = null;
             })
-            .catch(console.error);
+            .catch((e) => {
+              appState.error = e.message;
+              console.log(e);
+            });
         }
         break;
       }
