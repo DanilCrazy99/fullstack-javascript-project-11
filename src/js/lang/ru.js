bd2aad17bec3afe1f328a9edda6054ef9d281851
Danil Volkov <volchara287@gmail.com>
2024-03-20 00:32:32 +0400

    add: добавлен русский словарь для i18next
    Используется только для выводящихся скриптом текстов наподобии ошибки и информации о добавлении нового адреса в список

---
+++ src/js/lang/ru.js
@@ -0,0 +1,14 @@
+export default {
+  translation: {
+    feedback: {
+      errors: {
+        doubleUrl: 'В списке уже есть такой URL',
+        invalidUrl: 'Некорректный URL',
+        nullValue: 'URL не должен быть пустым',
+      },
+      info: {
+        urlAdded: 'URL успешно добавлен',
+      },
+    },
+  },
+};
