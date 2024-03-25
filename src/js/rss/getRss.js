cdf976f41922a23afecc4d58b67e5d72eabfc6a0
Danil Volkov <volchara287@gmail.com>
2024-03-25 23:25:08 +0400

    fix: меняет query параметр запроса к прокси для корректной работы
    Без параметра raw приходил другой объект требующий другой обработки для извлечения данных

--- src/js/rss/getRss.js
+++ src/js/rss/getRss.js
@@ -3,13 +3,14 @@ import parseRss from './parseRss.js';
 
 export default (url) =>
   new Promise((resolve, reject) => {
-    const allOriginsUrl = `https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`;
+    const allOriginsUrl = `https://allorigins.hexlet.app/raw?disableCache=true&url=${encodeURIComponent(url)}`;
     axios({
       method: 'get',
       url: allOriginsUrl,
       responseType: 'text',
     })
       .then((res) => {
+        console.log(res);
         const result = parseRss(res);
         resolve(result);
       })
