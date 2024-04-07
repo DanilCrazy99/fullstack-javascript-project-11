303015e8d7d9f409cc0c53c34b5bbae0038c1a34
Danil Volkov <volchara287@gmail.com>
2024-04-08 03:51:20 +0400

    Написать функцию которая стартует всё приложение #3

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,14 @@ import appState, { appStateInit } from './view/watcher.js';
 import { inputEl, btnEl, initFunc } from './view/components.js';
 import switcher from './lang/switcher.js';
 
-btnEl.addEventListener('click', (e) => {
-  e.preventDefault();
-  appState.value = inputEl.value;
-});
+const app = () => {
+  btnEl.addEventListener('click', (e) => {
+    e.preventDefault();
+    appState.value = inputEl.value;
+  });
 
-initFunc(appStateInit.feeds);
-switcher();
+  initFunc(appStateInit.feeds);
+  switcher();
+};
+
+app();
